68cc3660cc6ffc9931d1e9d7c8008a389422dbff
ly <330435863@qq.com>
2020-03-15 21:11:20 +0800

    修改帮助页面逻辑，实现未登录也可以查看帮助页面

--- public/templates/hibiscus/js/common.js
+++ public/templates/hibiscus/js/common.js
@@ -35,16 +35,6 @@ layui.define(['layer', 'laytpl', 'form', 'element','table'], function(exports){
 		}
 	};
 
-	console.group("欢迎使用ZFAKA开源开卡程序");
-		console.log("github地址：https://github.com/zlkbdotnet/zfaka/");
-		console.log("QQ交流群: 701035212");
-		console.log("欢迎前来围观、吐槽、点赞、捐赠、STAR......");
-	console.groupEnd();
-
-	console.group("作者信息");
-		console.log("网名：资料空白");
-		console.log("博客：http://zlkb.net");
-	console.groupEnd();
   //个人中心侧边导航
   var href = location.href + '/';
   var navItem = $('.fly-user-main .layui-nav .layui-nav-item');
